423adc782efa0d15c3f569472d57fcbb1984b306
DanielSzentimrey-Harrach <daniel.szhm@gmail.com>
2020-02-10 21:46:39 +0800

    Fixing failing unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,13 +19,13 @@ describe('AppComponent', () => {
   it(`should have as title 'test2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('test2');
+    expect(app.title).toEqual('test2a');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('test2 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('test2a app is running!');
   });
 });
