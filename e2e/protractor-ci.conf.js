b4a9c226c43ecdce6e2b1b192febe0ca9816e3aa
DanielSzentimrey-Harrach <daniel.szhm@gmail.com>
2020-02-16 12:31:40 +0800

    Some more parameters...

--- e2e/protractor-ci.conf.js
+++ e2e/protractor-ci.conf.js
@@ -3,7 +3,7 @@ const config = require('./protractor.conf').config;
 config.capabilities = {
     browserName: 'chrome',
     chomeOptions: {
-        args: ['--headless', '--no-sandbox', '--disable-gpu', "--disable-dev-shm-usage"]
+        args: ['--headless', '--no-sandbox', '--disable-gpu', "--disable-dev-shm-usage", "--remote-debugging-port=9222", "--remote-debugging-address=0.0.0.0"]
     }
 };
 
